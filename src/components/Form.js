92aa89ae9c71f4e1aaccca084adf9a692fb797ab
Lubnabd <lubna.abdelkhaleq@gmail.com>
2023-11-27 12:32:19 +0300

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,79 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    admin: false,
+
+  });
+
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value; 
+  /*const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+*/
+
+if (event.target.type === "checkbox") {
+  value = event.target.checked;
+}
+
+setFormData({
+  ...formData,
+  [name]: value,
+});
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(formData);
+}
+ /*
   function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+    setFormData({
+      ...formData,
+      firstName: event.target.value,
+    });
+  }
+    /*setFirstName(event.target.value);
   }
 
   function handleLastNameChange(event) {
-    setLastName(event.target.value);
+    setFormData({
+      ...formData,
+      lastName: event.target.value,
+    });
   }
+    //setLastName(event.target.value);
+  
+   
+    
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+    */
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+    <form onSubmit={handleSubmit} >
+      
+      <input type="text"   name="firstName"
+ onChange={handleChange} value={formData.firstName} />
+      <input type="text"   name="lastName"
+ onChange={handleChange} value={formData.lastName} />
+ <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
       <button type="submit">Submit</button>
+      
     </form>
   );
-}
 
+  }
 export default Form;
